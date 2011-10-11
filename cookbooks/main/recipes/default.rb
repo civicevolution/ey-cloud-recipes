627dbcceab16227e3f61747f7eedcdca89b0ffce
Brian Sullivan <brian@civicevolution.org>
2011-10-11 10:07:18 -0700

    Custom recipe to make node.js

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -6,7 +6,8 @@
 
 require_recipe 'fix_nginx_server_names'
 require_recipe 'daemons'
-require_recipe 'juggernaut'
+require_recipe 'nodejs'
+#require_recipe 'juggernaut'
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # require_recipe "sphinx"
