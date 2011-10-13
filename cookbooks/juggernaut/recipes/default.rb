f9d692b001536f66b83e0f8addcf5ee74bd6d3ac
Brian Sullivan <brian@civicevolution.org>
2011-10-12 17:14:56 -0700

    install init.d and monitrc for juggernaut

--- cookbooks/juggernaut/recipes/default.rb
+++ cookbooks/juggernaut/recipes/default.rb
@@ -6,6 +6,10 @@
 #
 # IMPORTANT: This has to run AFTER node.js is installed
 
+pidfile = '/var/run/juggernaut.pid'
+# I want to get the address of the DB MASTER from /etc/chef/dna.json
+redis_host = 'http://brian:123@ec2-50-18-101-127.us-west-1.compute.amazonaws.com:6379'
+
 if ['app','app_master','solo'].include?(node[:instance_role])
   install_dir     = "/usr/local/bin"
 
@@ -22,6 +26,47 @@ if ['app','app_master','solo'].include?(node[:instance_role])
     command "/sbin/start-stop-daemon --start --background --exec #{install_dir}/juggernaut --chuid root:root"
 #    not_if { FileTest.exists?("#{install_dir}/juggernaut") }
   end
+
+# install init.d
+  case node[:instance_role]
+    when "solo", "app_master"
+      template "/etc/init.d/jugggernaut-TEST" do
+        source "juggernaut.init.d.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({
+          :pid_file => pidfile
+        })
+      end
+  end
+
+
+
+  # add to monit
+  case node[:instance_role]
+    when "solo", "app_master"
+      template "/etc/monit.d/jugggernaut-TEST.monitrc" do
+        source "juggernaut.monitrc.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({
+          :pid_file => pidfile
+        })
+      end
+  end
+  
+ # # reload monit
+ # case node[:instance_role]
+ #   when "solo", "app_master"
+ #     execute "monit reload" do
+ #       action :run
+ #     end
+ # end
+
+
+
 
 end
 
