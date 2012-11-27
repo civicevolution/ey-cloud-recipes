f3d70c7818ca6f397c1b60530489c854d3c494b0
Brian Sullivan <brian@civicevolution.org>
2012-11-26 17:27:23 -0800

    Update chef recipe to install wkhtmltopdf

--- cookbooks/wkhtmltopdf/recipes/default.rb
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -12,7 +12,30 @@ if ['solo','app_master'].include?(node[:instance_role])
 
   execute "cd for wkhtmltopdf" do
     command "cd #{wkhtmltopdf_dir}"
-    command "curl http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2 | bunzip2 - | tar xvf -"
+    not_if { FileTest.exists?("#{wkhtmltopdf_dir}/wkhtmltopdf") }
+  end
+  
+  # download wkhtmltopdf 
+  remote_file "wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2" do
+    source "http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2"
+    owner 'root'
+    group 'root'
+    mode 0644
+    backup 0
+    not_if { FileTest.exists?("#{wkhtmltopdf_dir}/wkhtmltopdf") }
+  end
+
+  execute "unzip wkhtmltopdf" do
+    command "bunzip2 wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2 && sync"
+    not_if { FileTest.exists?("#{wkhtmltopdf_dir}/wkhtmltopdf") }
+  end
+
+  execute "unarchive wkhtmltopdf" do
+    command "tar xvf wkhtmltopdf-0.10.0_rc2-static-amd64.tar && sync"
+    not_if { FileTest.exists?("#{wkhtmltopdf_dir}/wkhtmltopdf") }
+  end
+
+  execute "cd for wkhtmltopdf" do
     command "mv wkhtmltopdf-amd64 wkhtmltopdf"
     not_if { FileTest.exists?("#{wkhtmltopdf_dir}/wkhtmltopdf") }
   end
